a435edf514a9f6843a00a969bdf98e89018b3bcf
kalifah0 <99051577+kalifah0@users.noreply.github.com>
2025-07-15 23:35:12 +0100

    fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -39,7 +39,7 @@ malvin({
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
           newsletterJid: "120363420656466131@newsletter",
-          newsletterName: "LUCKY-XD",
+          newsletterName: "THEDEVELOPER",
           serverMessageId: 143
         }
       }
