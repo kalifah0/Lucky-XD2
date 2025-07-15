2c989850753de7f0b8dc2d3220fde5d59d94542b
kalifah0 <99051577+kalifah0@users.noreply.github.com>
2025-07-15 23:44:37 +0100

    gitstalk.js

--- plugins/gitstalk.js
+++ plugins/gitstalk.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 👥 *Followers*: ${data.followers} | Following: ${data.following}
 📅 *Created At*: ${new Date(data.created_at).toDateString()}
 🔭 *Public Gists*: ${data.public_gists}
-> © Powered By Lucky Tech Hub`;
+> © Powered By Hassan Tech Hub`;
           const sentMsg = await conn.sendMessage(from,{image:{url: data.avatar_url },caption: userInfo },{quoted:mek })
     } catch (e) {
         console.log(e);
