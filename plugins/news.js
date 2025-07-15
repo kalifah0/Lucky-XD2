e65fd5c1692714c002a36a296f78fc5ed59711a5
kalifah0 <99051577+kalifah0@users.noreply.github.com>
2025-07-16 00:03:27 +0100

    news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-> ©Powered By Lucky Tech Hub
+> ©Powered By Hassan Tech Hub
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
