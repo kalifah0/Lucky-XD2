4a4479b69ff8e79c265aa9888d60434c833ca016
kalifah0 <99051577+kalifah0@users.noreply.github.com>
2025-07-16 00:28:38 +0100

    tool-pdf.js

--- plugins/tool-pdf.js
+++ plugins/tool-pdf.js
@@ -25,11 +25,11 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
             await conn.sendMessage(from, {
                 document: pdfData,
                 mimetype: 'application/pdf',
-                fileName: 'LuckyTech.pdf',
+                fileName: 'HassanTech.pdf',
                 caption: `
 *📄 PDF created successully!*
 
-> © Created By LUCKY XD 💜`
+> © Created By THEDEVELOPER 💜`
             }, { quoted: mek });
         });
 
