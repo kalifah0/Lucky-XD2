f6c7f610adc2f61c82e098ef1b82a61b13571dc4
kalifah0 <99051577+kalifah0@users.noreply.github.com>
2025-07-16 11:11:59 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -59,10 +59,10 @@ LINK_WHITELIST: "youtube.com,github.com",
         STICKER_NAME: process.env.STICKER_NAME || "THEDEVELOPER",
 // type sticker pack name 
 
-        CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
+        CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react  
   
-      CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+      CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,✍️,❤️,👋,✨,🇹🇷,🧑‍💻,🇳🇬,🤖,⚡,👍",
 // chose custom react emojis by yourself 
 
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
